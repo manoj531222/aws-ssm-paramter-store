137ed99d03a2d414e1e440f26202af80b643bc3b
manoj <gandhamanojkutty@gmail.com>
2023-05-11 20:44:49 +0530

    parametrs

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
   value = var.parameters[count.index].value
-  type        = "SecureString"
+  type        = "String"
   key_id = "d3361cbf-db50-4957-bbe4-05732c7ce6ed"
 }
